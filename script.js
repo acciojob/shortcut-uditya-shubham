79e8b3f563b692f434458ebda25e1c5b76666390
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-06-26 17:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ function shortcut(s1, s2) {
 // your code here
    
 
-if(s1.size==0)
-return s1;
-else if(s2.size==0)
-return s2;
-
-else
-return s[0]+s[1];
+if(s1=="" || s2.size==0)
+	return "";
+ 
+return s1[0]+s2[0];
 }
 
 // Do not change the code below.
